747c4bb00965ad6c193bfec335eff90eab5c151b
Arthur Diógenes <arthurdfleite@gmail.com>
2025-04-24 16:55:00 -0300

    clean

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function adicionarTexto() {
     let texto = document.getElementById("input");
-    document.querySelector("h1").innerHTML = texto.value;
+    document.querySelector("h1").innerText = texto.value;
 }
 
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
-  adicionarTexto();
-});
+btn.addEventListener("click", adicionarTexto);
